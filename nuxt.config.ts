4fcb75cd57c284043d8fd1dffcb5df5aab5216ec
haex <hexxx@ok.de>
2025-05-22 13:01:25 +0200

    start with shadcn vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
-const currentDir = dirname(fileURLToPath(import.meta.url))
+const currentDir = dirname(fileURLToPath(import.meta.url));
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
 
   devtools: { enabled: true },
 
@@ -15,24 +15,34 @@ export default defineNuxtConfig({
     "@pinia/nuxt",
     "@vueuse/nuxt",
     "@nuxt/icon",
-    "nuxt-snackbar",
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
   ],
 
   imports: {
     dirs: [
-      join(currentDir, './composables/**'),
-      join(currentDir, "./stores/**"),
+      join(currentDir, "./composables/**"),
       join(currentDir, "./components/**"),
-      join(currentDir, "./pages/**"),
-      join(currentDir, "./types/**")
+      join(currentDir, "./stores/**"),
     ],
+  },
 
+  colorMode: {
+    preference: "system", // default value of $colorMode.preference
+    fallback: "dark", // fallback value if not system preference found
+    hid: "nuxt-color-mode-script",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "",
+    classSuffix: "",
+    //storage: "localStorage", // or 'sessionStorage' or 'cookie'
+    storageKey: "nuxt-color-mode",
   },
 
   i18n: {
     strategy: "prefix_and_default",
     defaultLocale: "de",
-    vueI18n: join(currentDir, './i18n/i18n.config.ts'),
+    vueI18n: join(currentDir, "./i18n/i18n.config.ts"),
 
     locales: [
       { code: "de", language: "de-DE", isCatchallLocale: true },
@@ -58,18 +68,21 @@ export default defineNuxtConfig({
     },
   },
 
-  css: [join(currentDir, "./assets/css/main.css")],
+  css: [join(currentDir, "./assets/css/tailwind.css")],
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
 
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-    optimizeDeps: {
-      include: ['flyonui/flyonui'],
-    },
+    plugins: [tailwindcss()],
   },
-
-  build: {
-    transpile: ['flyonui'],
-  },
-})
+});
