cc5cace8a19517855a3e1ca2e1b07e8b832be46e
haex <hexxx@ok.de>
2025-05-22 14:28:15 +0200

    add tes dialog

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,40 @@
     <div class="text-5xl text-center">Haex Base Ui</div>
     <Button @click="onClick">Test</Button>
     <ThemeSwitcher />
-    <!-- <h1>Color mode: {{ colorMode.value }}</h1>
-    <h1>Preference {{ colorMode.preference }}</h1> -->
+    <Dialog>
+      <DialogTrigger as-child>
+        <Button variant="outline" @click="onClick">
+          Edit Profile
+        </Button>
+      </DialogTrigger>
+      <DialogContent class="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div class="grid gap-4 py-4">
+          <div class="grid grid-cols-4 items-center gap-4">
+            <Label for="name" class="text-right">
+              Name
+            </Label>
+            <Input id="name" default-value="Pedro Duarte" class="col-span-3" />
+          </div>
+          <div class="grid grid-cols-4 items-center gap-4">
+            <Label for="username" class="text-right">
+              Username
+            </Label>
+            <Input id="username" default-value="@peduarte" class="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">
+            Save changes
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   </div>
 </template>
 
